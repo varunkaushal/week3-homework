3b18233fae692a6258214d9f6af75b7b72654601
Varun Kaushal <varunkaushal@uchicago.edu>
2014-01-28 19:38:00 -0600

    Cleaned up code to use common method
    created common dice method and added comments

--- dice/app/controllers/dice_controller.rb
+++ dice/app/controllers/dice_controller.rb
@@ -6,26 +6,31 @@ class DiceController < ApplicationController #Inheritance for all controllers
 		return
 	end
 	
-	def start_game
+	def roll_the_dice # created the two dice and 'rolls' them to pick one side of each
 		dice1 = ['1', '2', '3', '4', '5', '6']
 		dice2 = ['1', '2', '3', '4', '5', '6']
 		
 		@d1 = dice1.sample(1)
 		@d2 = dice2.sample(1)
+	end
+	
+	def start_game
+	
+		roll_the_dice # rolls the dice
 		
-		@sum = @d1.first.to_i + @d2.first.to_i
+		@sum = @d1.first.to_i + @d2.first.to_i # sets total sum
 		
-		if  @sum == 7 or @sum == 11
+		if  @sum == 7 or @sum == 11 # initial win condition
 			render "win"
 			puts "roll 1 win"
 			return
 		end
 		
-		if  @sum == 2 or @sum == 3 or @sum == 12
+		if  @sum == 2 or @sum == 3 or @sum == 12 # initial lose condition
 			render "lose"
 			puts "roll 1 lose"
 			return
-		else
+		else # goal is set, roll again
 			render "rollagain"
 			puts "roll 1 rollagain"
 			return
@@ -34,27 +39,24 @@ class DiceController < ApplicationController #Inheritance for all controllers
 	
 	
 	def re_roll
-		dice1 = ['1', '2', '3', '4', '5', '6']
-		dice2 = ['1', '2', '3', '4', '5', '6']
+	
+		roll_the_dice # roll the dice
 		
-		@d1 = dice1.sample(1)
-		@d2 = dice2.sample(1)
+		@sum = params[:sum] # gets goal sum from params
 		
-		@sum = params[:sum]
-		puts @sum
-		@sum2 = @d1.first.to_i + @d2.first.to_i
+		@sum2 = @d1.first.to_i + @d2.first.to_i # calculate roll sum
 		
-		if  @sum2 == 7 
+		if  @sum2 == 7  # lose condition
 			render "lose"
 			puts "roll # lose"
 			return
 		end
 		
-		if  @sum.to_i == @sum2
+		if  @sum.to_i == @sum2 # roll matches goal, win condition
 			render "win"
 			puts "roll # win"
 			return
-		else
+		else # re-roll condition
 			render "rollagain"
 			puts "roll # rollagain"
 			return
